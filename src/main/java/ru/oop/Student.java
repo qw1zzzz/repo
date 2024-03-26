9ea066fe22f72bcaa6cd852cb95fcc9912c145c3
qw1z <larin.artem.al@gmail.com>
2024-03-26 23:53:45 +0300

    Student.java

---
+++ src/main/java/ru/oop/Student.java
@@ -0,0 +1,22 @@
+package ru.oop;
+
+public class Student {
+
+    public void music(){
+        System.out.println("La la la");
+    }
+
+    public void sing(){
+        System.out.println("I beleve i can fly");
+    }
+
+    public static void main(String[] args){
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.sing();
+        petya.sing();
+        petya.sing();
+    }
+}
