a8aec27f2213a391e0f66080d5ea91067f186a3f
qw1z <larin.artem.al@gmail.com>
2024-03-28 12:03:52 +0300

    7.5 Вызов метода с возвращаемым типом/ 28.03.2024

--- src/main/java/ru/oop/Cat.java
+++ src/main/java/ru/oop/Cat.java
@@ -2,9 +2,15 @@ package ru.oop;
 
 public class Cat
 {
+    public String sound(){
+        String voice = "miu-miu";
+        return voice;
+    }
     public static void main(String[] args)
     {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
+        String say = peppy.sound();
+        System.out.println("Peppy says " + say);
     }
 }
---
+++ src/main/java/ru/oop/DymmyDic.java
@@ -0,0 +1,7 @@
+package ru.oop;
+
+public class DymmyDic {
+    public String engToRus(String eng) {
+        return "Неизвестное слово." + eng;
+    }
+}
