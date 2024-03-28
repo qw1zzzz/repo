b4aa6cd7651814c6e81c748f032f8ddd3a978032
qw1z <larin.artem.al@gmail.com>
2024-03-28 14:52:17 +0300

    Поля объекта/28.03.24

--- src/main/java/ru/oop/Cat.java
+++ src/main/java/ru/oop/Cat.java
@@ -1,16 +1,39 @@
 package ru.oop;
 
-public class Cat
-{
-    public String sound(){
+public class Cat {
+    private String food;
+    private String name;
+    public void eat(String meat) {
+        this.food = meat;
+    }
+    public void giveNick(String nick) {
+        this.name = nick;
+
+    }
+    public void show() {
+        System.out.println(this.name + " eats " + this.food);
+    }
+    public String sound() {
         String voice = "miu-miu";
         return voice;
     }
-    public static void main(String[] args)
-    {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+    public static void main(String[] args) {
+        System.out.println("There is gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("gav");
+        gav.show();
+
+        System.out.println("There is black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("black");
+        black.show();
+
+
+//        Cat peppy = new Cat();
+//        Cat sparky = new Cat();
+//        String say = peppy.sound();
+//        System.out.println("Peppy says " + say);
     }
 }
