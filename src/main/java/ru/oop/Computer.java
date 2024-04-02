4bfdefb5c2aba02b8deed4dc787c66f0d0216dc4
qw1z <larin.artem.al@gmail.com>
2024-04-02 16:15:59 +0300

    7.9 Пергрузка конструктора / 02.04.24

--- src/main/java/ru/oop/Computer.java
+++ src/main/java/ru/oop/Computer.java
@@ -5,29 +5,41 @@ public class Computer {
     private int ssd;
     private String cpu;
 
+    public Computer() {
+
+    }
+
     public Computer(boolean multiMonitor, int ssd, String cpu) {
         this.multiMonitor = multiMonitor;
         this.ssd = ssd;
         this.cpu = cpu;
     }
 
-    public Computer() {
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
+        this.cpu = cpu;
     }
 
     public void printInfo() {
-        System.out.println("Много мониторов: " + multiMonitor);
+        System.out.println("MultiMonitor: " + multiMonitor);
         System.out.println("SSD: " + ssd + " GB");
-        System.out.println("Модель CPU: " + cpu);
+        System.out.println("CPU: " + cpu);
+        System.out.println();
     }
-
     public static void main(String[] args) {
-        Computer computer = new Computer(true, 512, "Intel Core i7-13500k");
-        computer.printInfo();
-        System.out.println();
-        Computer comp = new Computer(false, 1024, "AMD Ryzen 5600X");
-        comp.printInfo();
-        System.out.println();
-        Computer zeroComp = new Computer();
-        zeroComp.printInfo();
+        Computer first = new Computer();
+        first.printInfo();
+        Computer second = new Computer(true, 512, "AMD Ryzen 5");
+        second.printInfo();
+        Computer third = new Computer(1024, "Intel Core i7-13700k");
+        third.printInfo();
+        Computer forth = new Computer(false, 512.0, "Amd Ryzen 5600X");
+        forth.printInfo();
     }
 }
